e7884e1c876e3201a664afcc73c324b07500906a
nikolapratte <nikolapratte@gmail.com>
2021-08-27 12:58:31 -0700

    tests: db player functions

--- tests/test_ladder_database.py
+++ tests/test_ladder_database.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import pytest
 
@@ -8,16 +9,50 @@ from ..ladder_manager.constants import test_db_filename
 
 @pytest.fixture
 def test_db():
-    db = LadderDB()
-
-    db.setup_ladder(test_db_filename)
+    db = LadderDB(test_db_filename)
 
     yield db
 
     # cleanup generated file
+
+    db.con.close()
     os.remove(test_db_filename)
 
 
-def test_database_file_exists_after_creation(test_db):
+def test_database_file_exists_after_creation(test_db: LadderDB):
     assert os.path.exists(test_db_filename)
 
+
+def test_database_doesnt_crash_if_exists():
+    db = LadderDB(test_db_filename)
+    del db
+
+    db2 = LadderDB(test_db_filename)
+
+    db2.con.close()
+    os.remove(test_db_filename)
+
+
+
+def test_player_exists(test_db: LadderDB):
+    assert test_db._player_exists(124) is False
+
+
+def test_player_exists_true(test_db: LadderDB):
+    test_db.cur.execute("INSERT INTO players VALUES (42, 7000)")
+    assert test_db._player_exists(42) is True
+
+
+def test_player_create(test_db: LadderDB):
+    test_db._create_player(2)
+    assert test_db._player_exists(2) is True
+
+
+def test_player_get_rating(test_db: LadderDB):
+    test_db._create_player(3495)
+    assert test_db._get_player_rating(3495) == LadderDB.starting_rating
+
+
+def test_player_get_rating_exception_player_does_not_exist(test_db: LadderDB):
+    with pytest.raises(ValueError):
+        test_db._get_player_rating(432)
