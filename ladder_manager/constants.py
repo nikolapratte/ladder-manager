9a6048f2e691368f1e6f8ab893b5dbd2316d63ec
nikolapratte <nikolapratte@gmail.com>
2021-08-31 07:09:04 -0700

    add: help command related info

--- ladder_manager/constants.py
+++ ladder_manager/constants.py
@@ -6,13 +6,30 @@ from .Command import Command
 from .CommandInformation import CommandInformation, ReportInformation
 
 
+cancel_emoji = "❌"
+
+command_alternative_names = {
+    Command.help: "h"
+}
 
 command_names = {Command.help: "Help",
+    Command.leaderboard: "Leaderboard",
     Command.report: "Report"}
+
+command_descriptions = {
+    Command.cancel: "Cancels the current command.",
+    Command.help: "List of commands and short descriptions of what they do.",
+    Command.leaderboard: "Shows leaderboard of players",
+    Command.report: "Report a match."
+}
 
 # Mapping between commands and what information they need.
 # If a command doesn't need extra information, will have None.
-command_to_information = {Command.report: ReportInformation}
+command_to_information = {
+    Command.report: ReportInformation,
+    Command.help: None,
+    Command.leaderboard: None
+}
 command_symbol = "!"
 
 delete_msgs_after = 10
@@ -20,7 +37,7 @@ delete_msgs_after = 10
 # will be deprecated once do db per server/channel.
 main_ladder_name = "main_ladder.db"
 
-timeout_message = "Sorry, your session timed out. Please try again."
+timeout_message = "Sorry, your session was cancelled or timed out. Please try again."
 timeout_limit = 15
 token_filename = "ladder_manager/token.txt"
 
